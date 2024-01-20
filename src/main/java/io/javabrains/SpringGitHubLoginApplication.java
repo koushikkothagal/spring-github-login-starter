19634d8f15b25812fa476399ebb5ebf4798a864c
Naman <jhanaman571@gmail.com>
2024-01-20 16:52:22 +0530

    Updated to spring security 6 and made necessary changes.

--- src/main/java/io/javabrains/SpringGitHubLoginApplication.java
+++ src/main/java/io/javabrains/SpringGitHubLoginApplication.java
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringGitHubLoginApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGitHubLoginApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitHubLoginApplication.class, args);
+    }
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
-	}
-	
+    @RequestMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal);
+        return principal.getAttribute("login");
+    }
 
 }
